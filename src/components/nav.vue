8954083d8af0f9f4771c7f18a801860fb2154519
asp-cc <2598772546@qq.com>
2020-02-20 21:24:29 +0800

    modify img

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,20 +1,13 @@
 <template>
   <div id="nav">
-    <el-menu
-              :default-active="activeIndex2"
-              class="el-menu-demo"
-              mode="horizontal"
-              @select="handleSelect"
-              background-color="#545c64"
-              text-color="#fff"
-              active-text-color="#ffd04b">
-        <el-menu-item v-for="item in navList" :index="++count" :style="[listStyleObj]">
-            <template v-for="site,name in item">
-                <a :href="site" :style="[archStyleObj]">{{ name }}</a>
-            </template>
-        </el-menu-item>
-    </el-menu>
-
+      <ul>
+        <li style="padding:.8% 0 0 10%;margin-right:10%;"><img src="../assets/img/python.jpg" width="80" /></li>
+        <li v-for="item in navList">
+          <template v-for="site,name in item">
+            <a :href="site">{{ name }}</a>
+          </template>
+        </li>
+      </ul>
   </div>
 </template>
 
@@ -45,4 +38,29 @@ export default {
 </script>
 
 <style scoped>
+  ul {
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+  width: 100%;
+  background-color: white;
+  overflow: hidden;
+}
+  li {
+  float: left;
+  overflow: hidden;
+}
+  a {
+  text-decoration: none;
+  display: block;
+  padding: 2em 1.5em;
+  font-size: 1.2em;
+  transition: all .5s;
+  color: black;
+  border-radius: 20px;
+}
+  a:hover {
+  background-color: orange;
+  color: white;
+}
 </style>
