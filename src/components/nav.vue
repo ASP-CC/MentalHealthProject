d87a725ced3e08b6bb441c7492bfdc19c1ffcb42
asp-cc <2598772546@qq.com>
2020-02-20 17:37:12 +0800

    basic components

---
+++ src/components/nav.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="nav">
+    <el-menu
+      :default-active="activeIndex2"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+
+        <el-menu-item index="1">
+          <a href="../index/index.html">首页</a>
+        </el-menu-item>
+
+        <el-menu-item index="2">
+          <a href="../about/index.html">关于</a>
+        </el-menu-item>
+
+        <el-menu-item index="3">
+          <a href="../mental_equip/index.html">心理设备</a>
+        </el-menu-item>
+
+        <el-menu-item index="4">高端心理</el-menu-item>
+    </el-menu>
+    <ul>
+      <li v-for="item in navList">
+          {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+      data(){
+        return {
+          navList: ["首页", "关于", ]
+        }
+      }
+  }
+</script>
+
+<style>
+a {
+  text-decoration: "none";
+}
+</style>
--- src/mental_equip/index.js
+++ src/mental_equip/index.js
@@ -1,16 +1,15 @@
 import Vue from 'vue'
-// import App from './App.vue'
-import Nav from './components/nav'
-import router from './router'
+import Nav from '../components/nav'
+import router from '../router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
-// new Vue({
-//   router,
-//   render: h => h(App)
-// }).$mount('#ap')
+
 new Vue({
   router,
   render: h => h(Nav)
 }).$mount('#nav')
+
+
